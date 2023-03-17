8b3d2a850ac4dc2f60c5afd8811632ac2f42e174
Ahmad Mustafeen <engr.ahmadmustafeen@gmail.com>
2023-03-17 17:26:28 +0500

    added image generation

--- src/routes/apis/index.ts
+++ src/routes/apis/index.ts
@@ -21,37 +21,6 @@ const messages: {
     role: ChatCompletionRequestMessageRoleEnum.System,
     content: "article must contain atleast 4 headings",
   },
-
-  // {
-  //   role: ChatCompletionRequestMessageRoleEnum.System,
-  //   content:
-  //     "We are writing article to gain engagements for our software house",
-  // },
-  // {
-  //   role: ChatCompletionRequestMessageRoleEnum.System,
-  //   content: "Name of our software house is Geeks Of Kolachi",
-  // },
-  // {
-  //   role: ChatCompletionRequestMessageRoleEnum.System,
-  //   content:
-  //     "Geeks Of Kolachi is a US based tech startup which helps startup to scale their application to the next level",
-  // },
-  // {
-  //   role: ChatCompletionRequestMessageRoleEnum.System,
-  //   content:
-  //     "We offer Web/Mobile Application development, Custom Software solutions, Graphic designing",
-  // },
-  {
-    role: ChatCompletionRequestMessageRoleEnum.System,
-    content:
-      "write the heading and wrap that with h1 tag and paragragh with p tag",
-  },
-
-  {
-    role: ChatCompletionRequestMessageRoleEnum.System,
-    content:
-      "write the conclusion, Geeks of kolachi is a software companies helping startups to scale their application to the next level",
-  },
 ];
 
 router.post("/", async (req: Request, res: Response) => {
@@ -75,6 +44,20 @@ router.post("/", async (req: Request, res: Response) => {
     });
     let data = completion.data || "";
     res.status(200).send(data?.choices?.[0]?.message?.content || "");
+  } catch (err) {
+    console.log({ err });
+    res.status(502).send("ERROR");
+  }
+});
+
+router.post("/image-variations", async (req: Request, res: Response) => {
+  const query = req.body.query;
+  try {
+    const completion = await openaiApi.createImage({
+      prompt: query,
+    });
+    let data = completion.data || "";
+    res.status(200).send(data.data);
   } catch (err) {
     console.log({ err });
     res.status(502).send("ERROR");
