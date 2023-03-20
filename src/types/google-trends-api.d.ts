99249458c661340a2d3cdd4c1f7c192724466257
Ahmad Mustafeen <engr.ahmadmustafeen@gmail.com>
2023-03-20 15:55:30 +0500

    added google trends api

---
+++ src/types/google-trends-api.d.ts
@@ -0,0 +1,33 @@
+declare module "google-trends-api" {
+  export interface TrendOptions {
+    keyword: string | string[];
+    startTime?: Date;
+    endTime?: Date;
+    geo?: string | string[];
+    hl?: string;
+    timezone?: number;
+    category?: number;
+    property?: string;
+    granularTimeResolution?: boolean;
+  }
+
+  interface InterestOverTimeResult {
+    default: {
+      timelineData: {
+        time: string;
+        formattedTime: string;
+        formattedAxisTime: string;
+        value: number[];
+        formattedValue: string[];
+      }[];
+    };
+  }
+
+  export function interestOverTime(
+    options: TrendOptions
+  ): Promise<InterestOverTimeResult>;
+
+  export function interestByRegion(options: any): Promise<any>;
+
+  export function realTimeTrends(options: any): Promise<any>;
+}
